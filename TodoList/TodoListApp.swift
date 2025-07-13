6b4a646ce61ecfe7aec17313afffe959390f58e9
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-13 11:55:47 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Thiago Ogawa on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
