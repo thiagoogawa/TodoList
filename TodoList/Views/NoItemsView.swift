896b586e5ecc06ec473826077553b7d3fde257c7
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-17 18:17:11 -0300

    feat: add animation function and apply it to NoItemsView

--- TodoList/Views/NoItemsView.swift
+++ TodoList/Views/NoItemsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
@@ -15,6 +18,7 @@ struct NoItemsView: View {
                     .font(.title)
                     .fontWeight(.semibold)
                 Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
                 NavigationLink(
                     destination: AddView(),
                     label: {
@@ -23,14 +27,36 @@ struct NoItemsView: View {
                             .font(.headline)
                             .frame(height: 55)
                             .frame(maxWidth: .infinity)
-                            .background(Color.accentColor)
+                            .background(animate ? Color.red : Color.accentColor)
                             .cornerRadius(10)
                     })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 15 : 10,
+                    x: 0,
+                    y: animate ? 20: 10)
+                .scaleEffect(animate ? 1.1 : 1)
+                .offset(y: animate ? -7 : 0)
             }
             .multilineTextAlignment(.center)
             .padding(40)
+            .onAppear(perform: addAnimation)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
     }
 }
 
