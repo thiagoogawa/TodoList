4cfb280ded1f68f1bb657a7092b5836318c32a7e
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-17 17:53:01 -0300

    feat: create NoItemsView

--- TodoList/Views/NoItemsView.swift
+++ TodoList/Views/NoItemsView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct NoItemsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                NavigationLink(
+                    destination: AddView(),
+                    label: {
+                        Text("Add something 🥳")
+                            .foregroundStyle(.white)
+                            .font(.headline)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.accentColor)
+                            .cornerRadius(10)
+                    })
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
 #Preview {
-    NoItemsView()
+    NavigationView {
+        NoItemsView()
+            .navigationBarTitle("No Items")
+    }
 }
