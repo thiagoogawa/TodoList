6e46cb88695ef82cce9a52af37a2c76ca57f8244
Thiago Ogawa <thiago.ogawa11@gmail.com>
2025-07-17 17:52:14 -0300

    feat: create NoItemsView

---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Thiago Ogawa on 17/07/25.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
